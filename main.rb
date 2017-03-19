96dca3ce857595e3713fc0c09e7a9b14f62c5c0d
edwinwells <edwin.wallace@yandex.com>
2017-03-18 23:00:54 -0400

    All possible
    win scenarios are installed.

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ newboard=Board.new
 terminal=Terminal.new
  
 newboard={
-			:a1=>"X", :a2=>"O", :a3=>"X",
+			:a1=>"O", :a2=>"O", :a3=>"O",
 		 	:b1=>"X", :b2=>"", :b3=>"O",
 		  	:c1=>"X", :c2=>"O", :c3=>"X"	
 		 }
--- terminal.rb
+++ terminal.rb
@@ -32,24 +32,140 @@ end
 		    empty[:a2] == "X" &&
 		   	empty[:a3] == "X"
 		   	) == true
-			puts""
-			puts " X wins"
+
+			win_for_X()
+
 		elsif (
-			empty[:a1] == "X" &&
-		    empty[:b1] == "X" &&
-		   	empty[:c1] == "X"
-		   	) == true
+			   empty[:a1] == "X" &&
+		       empty[:b1] == "X" &&
+		   	   empty[:c1] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			   empty[:a3] == "X" &&
+		       empty[:b3] == "X" &&
+		   	   empty[:c3] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			   empty[:c3] == "X" &&
+		       empty[:c2] == "X" &&
+		   	   empty[:c1] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			   empty[:b3] == "X" &&
+		       empty[:b2] == "X" &&
+		   	   empty[:b1] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			   empty[:a1] == "X" &&
+		       empty[:b2] == "X" &&
+		   	   empty[:c3] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			   empty[:a3] == "X" &&
+		       empty[:b2] == "X" &&
+		   	   empty[:c1] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			   empty[:a2] == "X" &&
+		       empty[:b2] == "X" &&
+		   	   empty[:c2] == "X"
+		   	  ) == true
+
+			  win_for_X()
+
+		elsif (
+			    empty[:a1] == "O" &&
+		        empty[:a2] == "O" &&
+		   	    empty[:a3] == "O"
+		   	   ) == true
+
+			   win_for_O()
+
+		elsif (
+			   empty[:a1] == "O" &&
+		       empty[:b1] == "O" &&
+		   	   empty[:c1] == "O"
+		   	  ) == true
+
+			  win_for_O()
+
+		elsif (
+			   empty[:a3] == "O" &&
+		       empty[:b3] == "O" &&
+		   	   empty[:c3] == "O"
+		   	  ) == true
+
+			  win_for_O()
+
+		elsif (
+			   empty[:c3] == "O" &&
+		       empty[:c2] == "O" &&
+		   	   empty[:c1] == "O"
+		   	  ) == true
+
+			  win_for_O()
+
+		elsif (
+			   empty[:b3] == "O" &&
+		       empty[:b2] == "O" &&
+		   	   empty[:b1] == "O"
+		   	  ) == true
+
+			  win_for_O()
+
+		elsif (
+			   empty[:a1] == "O" &&
+		       empty[:b2] == "O" &&
+		   	   empty[:c3] == "O"
+		   	  ) == true
+
+			  win_for_O()
+
+		elsif (
+			   empty[:a3] == "O" &&
+		       empty[:b2] == "O" &&
+		   	   empty[:c1] == "O"
+		   	  ) == true
+
+			  win_for_O()
+
+		elsif (
+			   empty[:a2] == "O" &&
+		       empty[:b2] == "O" &&
+		   	   empty[:c2] == "O"
+		   	  ) == true
+
+			  win_for_O()																
+		end
+	end
+
+		def win_for_X()
 			puts""
 			puts " X wins"
-		
-
-
 		end
 
-
-
-
-	end
+		def win_for_O()
+			puts""
+			puts " O wins"
+		end
 
 	def showboard(empty)
 	        puts ""
