2d5f96e2f75226b6e2fdf7b586b6071f57ebaf1c
edwinwells <edwin.wallace@yandex.com>
2017-03-22 17:52:11 -0400

    Complete with
    'tie-game' statement, and choice for rematch!

--- human_v_human.rb
+++ human_v_human.rb
@@ -16,32 +16,47 @@ terminal = Terminal.new
 			newgame.get_X()
 			terminal.check_for_wins(newgame.setup)
 			terminal.showboard(newgame.setup)
-			if newgame.check_full? == true ||
-			  terminal.win == true
+			if newgame.check_full? == true
+				  print " The game is tie! Care for another game? (Y or N) "
+				  again = gets.chomp
+			  	if again.downcase == "y"
+			  		startgame()
+			  	else 
+			  		exit
+			  	end
+			end
+			if	  terminal.win == true
 				  print " Care for another game? (Y or N) "
 				  again = gets.chomp
+				  if again.downcase == "y"
+				  	startgame()
+				  else 
+				  	exit
+				  end			
+			end	
 
+			newgame.get_O()
+			terminal.check_for_wins(newgame.setup)
+			terminal.showboard(newgame.setup)
+			if newgame.check_full? == true
+				  print " The game is tie! Care for another game? (Y or N) "
+				  again = gets.chomp
+				  	if again.downcase == "y"
+				  		startgame()
+				  	else 
+				  		exit
+				  	end
+			end
+			if	  terminal.win == true
+				  print " Care for another game? (Y or N) "
+				  again = gets.chomp
 
 				  	if again.downcase == "y"
 				  		startgame()
 				  	else 
 				  		exit
 				  	end			
-			end	
-
-			newgame.get_O()
-			terminal.check_for_wins(newgame.setup)
-			terminal.showboard(newgame.setup)
-			if newgame.check_full? == true ||
-			  terminal.win == true
-			  print " Care for another game? (Y or N) "
-			  again = gets.chomp
-			  	if again.downcase == "y"
-			  		startgame()
-			  	else
-			  	    exit
-			  	end			
-			end	
+			end
 		end
 end
 startgame()
