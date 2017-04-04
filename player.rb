1dda6f3526301de42e8d8a0dbd2811b7e1d084cf
edwinwells <edwin.wallace@yandex.com>
2017-04-03 20:13:23 -0400

    Removed ' def initialize' from subclasses of Player, as that attribute is inherited.

--- player.rb
+++ player.rb
@@ -8,24 +8,15 @@ end
 
 class ComputerSequential < Player
 
-	def initialize(input_name)
-		@player = input_name
-	end
 
 end
 
 class ComputerRandom < Player
 
-	def initialize(input_name)
-		@player = input_name
-	end
-	
+
 end
 
 class ComputerUnbeatable < Player
 
-	def initialize(input_name)
-		@player = input_name
-	end
 	
 end
