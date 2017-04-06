fc3fc0e336e41f5b525847d58f9d6c9aa26d9cdf
edwinwells <edwin.wallace@yandex.com>
2017-04-06 13:20:36 -0400

    Defined new class Game;
    located in game.rb.

---
+++ game.rb
@@ -0,0 +1,14 @@
+require_relative "gameboard.rb"
+require_relative "player.rb"
+
+
+class Game
+
+newboard = GameBoard.new()
+playerX = Player.new("X")
+playerO = ComputerRandom.new("O")
+
+
+end
+
+game = Game.new
