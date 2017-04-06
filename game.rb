ae05e795d8956f9c6b589211502d63c10d601ac0
edwinwells <edwin.wallace@yandex.com>
2017-04-06 13:27:41 -0400

    Created stub play() method in game.rb.

--- game.rb
+++ game.rb
@@ -8,7 +8,18 @@ newboard = GameBoard.new()
 playerX = Player.new("X")
 playerO = ComputerRandom.new("O")
 
+	def play()
+		a = 0
+		until a == 5 do
+			puts "hello"
+			a += 1
+		end
+
+	end
 
 end
 
 game = Game.new
+game.play()
+
+
