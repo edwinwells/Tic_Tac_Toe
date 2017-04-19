f0e73a12f1686375beef12aba0de49b0d855abcc
edwinwells <edwin.wallace@yandex.com>
2017-04-19 10:31:32 -0400

    Modified app.rb (require_relative(s)).

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 require_relative 'unbeatable.rb'
+require_relative "gameboard.rb"
+require_relative "terminal.rb"
+require_relative "player.rb"
+require_relative "game.rb"
 
 get '/' do
     erb :ttt
