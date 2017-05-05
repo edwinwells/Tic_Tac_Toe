a640bd62e583e9f1c2bc0005573f7879e3dc6401
edwinwells <edwin.wallace@yandex.com>
2017-05-05 07:39:01 -0400

    Stage 6

--- app.rb
+++ app.rb
@@ -5,19 +5,34 @@ require_relative "terminal.rb"
 require_relative "player.rb"
 require_relative "game.rb"
 
+# enable :sessions
+
 get '/' do
     erb :initial_page
 end
 
 get '/X' do
-newboard = GameBoard.new
-newboard={
+
+		playerX = Player.new("X")
+		playerO = ComputerUnbeatable.new("O")
+		reset = "N"
+
+		freshgame = VersusUnbeatable.new(playerX, playerO, reset)
+
+	freshgame = {
+				a1: "X", a2: "", a3: "",
+			 	b1: "", b2: "", b3: "",
+			  	c1: "", c2: "", c3: ""	
+			       }
+
+legend = GameBoard.new
+legend={
 			a1: "a1", a2: "a2", a3: "a3",
 		 	b1: "b1", b2: "b2", b3: "b3",
 		  	c1: "c1", c2: "c2", c3: "c3"	
 		 }
 
-    erb :User_Chooses_X, :locals => {newboard: newboard}
+    erb :User_Chooses_X, :locals => {freshgame: freshgame, legend: legend}
 end
 
 get '/O' do
@@ -27,50 +42,55 @@ end
 
 
 post '/humanX(move)' do
+	# current_game.set_position
+
+
+	# @setup[:"#{position}"] = character
+
+	freshgame = params[:freshgame]
+    legend = params[:legend]
     position = params[:move]
-    newboard = params[:newboard]
+    playerX = params[:playerX]
+    playerO = params[:playerO]
+
+	freshgame = VersusUnbeatable.new(playerX, playerO, "No")
+
+	# freshgame = {
+	# 			a1: "X", a2: "", a3: "",
+	# 		 	b1: "", b2: "", b3: "",
+	# 		  	c1: "", c2: "", c3: ""	
+	# 	        }    
+
+	freshgame.run_game(playerX, playerO, "X")
+
+# 
+    # puts current_game
+
     # result = startgame(place)
 	# newboard.set_position(position, "X")
-	# current_setup = params[:setup]
-newboard={
+	if position != "a1" ||
+		position != "a2" ||
+		position != "a3" ||
+		position != "b1" ||
+		position != "b2" ||
+		position != "b3" ||
+		position != "c1" ||
+		position != "c2" ||
+		position != "c3"
+		puts "Invalid entry! Please try again."
+	end
+
+# freshgame = {
+# 			a1: "X", a2: "", a3: "",
+# 		 	b1: "", b2: "", b3: "",
+# 		  	c1: "", c2: "", c3: ""	
+# 		       }
+legend = {
 			a1: "a1", a2: "a2", a3: "a3",
 		 	b1: "b1", b2: "b2", b3: "b3",
 		  	c1: "c1", c2: "c2", c3: "c3"	
 		 }
-    erb :User_Chooses_X, :locals => {newboard: newboard} 
+
+    erb :User_Chooses_X, :locals => {freshgame: freshgame, legend: legend} 
 
 end
-
-
-# post '/startgame(response)' do
-# 	response = params[:response]
-# 	erb :showinfo, :locals => {:responsw => response}
-# end
-
-# get '/' do
-#     response = ""
-# 	puts""
- 		
-# 	until response == "X" || response == "O" do
-# 		print " Will you choose to play as X or O? \n Enter X or O: "
-# 		response = gets.chomp.upcase
-# 		puts ""
-#  	end
-# end
-
-
-
-# post '/unbeatable.rb/startgame()' do
-# # 	number = params[:changevalue]
-# # 	result = exact_change(number)
-# # 	# "OK, for #{number}, you will need #{result}!  Get to countin'!!"
-# # 	erb :showinfo, :locals => {:number => number, 
-# # 		                       :result => result}
-# # 		              end
-
-
-# # get '/startgame()' do
-
-# 	erb :showinfo, :locals => {:response => response}
-
-# end
