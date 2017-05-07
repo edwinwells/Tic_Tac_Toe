706cf40028a28ed71ce934ac5b9d597b9ed041d6
edwinwells <edwin.wallace@yandex.com>
2017-05-07 17:05:38 -0400

    Pressing forward...

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ get '/X' do
 	session[:gameboard] = GameBoard.new
 
 
-    erb :User_Chooses_X, :locals => {playerX: session[:playerX], playerO: session[:playerO],freshgame: session[:freshgame], reset: session[:reset], gameboard: session[:gameboard]}
+    erb :HumanX_move, :locals => {playerX: session[:playerX], playerO: session[:playerO],freshgame: session[:freshgame], reset: session[:reset], gameboard: session[:gameboard]}
 end
 
 get '/O' do
@@ -30,62 +30,41 @@ get '/O' do
 	session[:reset] = "N"	
 	session[:freshgame] = VersusUnbeatable.new(session[:playerX], session[:playerO], session[:reset])
 	session[:gameboard] = GameBoard.new	
-    erb :User_Chooses_O
+    erb :HumanO_move
 end
 
 
 
-post '/humanX(move1)' do
+post '/humanX_move' do
 	# current_game.set_position
 
 	# @setup[:"#{position}"] = character
 
-	move1 = params[:move1]
+	move = params[:move]
 	freshgame = params[:freshgame]
     playerX = params[:playerX]
     playerO = params[:playerO]
     gameboard = params[:gameboard]
 
-	session[:gameboard].set_position(move1, "X")
+	session[:gameboard].set_position(move, "X")
 
-    # freshgame = freshgame
-
- #    a1 = freshgame[:a1]
- #    b1 = freshgame[:b1]
- #    c1 = freshgame[:c1]
- #    a2 = freshgame[:a2]
- #    b2 = freshgame[:b2]
- #    c2 = freshgame[:c2]
- #    a3 = freshgame[:a3]
- #    b3 = freshgame[:b3]
- #    c3 = freshgame[:c3]
-
-
-
-	# if position != "a1" ||
-	# 	position != "a2" ||
-	# 	position != "a3" ||
-	# 	position != "b1" ||
-	# 	position != "b2" ||
-	# 	position != "b3" ||
-	# 	position != "c1" ||
-	# 	position != "c2" ||
-	# 	position != "c3"
-	# 	puts "Invalid entry! Please try again."
-	# end
-
-    erb :ComputerO_move1, :locals => {:move1 => session[:move1], playerX: session[:playerX], playerO: session[:playerO],freshgame: session[:freshgame], reset: session[:reset], gameboard: session[:gameboard]}
+    erb :ComputerO_move, :locals => {:move => session[:move], playerX: session[:playerX], playerO: session[:playerO],freshgame: session[:freshgame], reset: session[:reset], gameboard: session[:gameboard]}
 end
 
- post '/humanX_move2' do
- 	computermove1 = session[:computermove1] 
-	move1 = session[:move1]
+post '/ComputerO_move' do
+	# current_game.set_position
+
+	# @setup[:"#{position}"] = character
+
+	move = params[:move]
 	freshgame = params[:freshgame]
     playerX = params[:playerX]
     playerO = params[:playerO]
     gameboard = params[:gameboard]
 
+	session[:gameboard].set_position(move, "O")
 
-    erb :humanX_move2, :locals => {:computermove1 => session[:computermove1], :move1 => session[:move1], :playerX => session[:playerX], :playerO => session[:playerO],:freshgame => session[:freshgame], :reset =>session[:reset]}
- end
+    erb :ComputerO_move, :locals => {:move => session[:move], playerX: session[:playerX], playerO: session[:playerO],freshgame: session[:freshgame], reset: session[:reset], gameboard: session[:gameboard]}
+end
+
 end
